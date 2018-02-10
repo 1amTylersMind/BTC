758c888dba3bfaa325da12514e257af5b6832afb
root <srobbin1@ithaca.edu>
2018-02-10 04:35:24 +0000

    Developing RNN in Spyder

--- spyderNoteBook.py
+++ spyderNoteBook.py
@@ -13,14 +13,17 @@ Created on Thu Feb  8 15:07:26 2018
 @author: Scott Robbins
 @date: 2/8/2017
 """
+# Imports 
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 import csv
 import math
 
 
 ################################ FUNCTIONS ####################################
+
+
 '''
 ANALYZE SINGLE MARKET
 '''
@@ -70,8 +73,8 @@ def getMarketParameters(fname):
 ############################# END OF FUNCTIONS ################################
 
 # Three Main Historic DataSets [Price][Volume][Volatility]
-pdat     = pd.read_csv('allMkt30d.csv').iloc[:,1:].values
-vdat   = pd.read_csv('allVol30d.csv').iloc[:,1:].values
+pdat = pd.read_csv('allMkt30d.csv').iloc[:,1:].values
+vdat = pd.read_csv('allVol30d.csv').iloc[:,1:].values
 tdat = pd.read_csv('tpmhrs30d.csv').iloc[:,1:].values
 dates = pd.read_csv('allMkt30d.csv').iloc[:,0].values
 
@@ -81,9 +84,9 @@ volParams   = getMarketParameters('allvol30d.csv')
 tradeParams = getMarketParameters('tpmhrs30d.csv')
 
 # Get the 30 Day moving average Data Frames
-mavgPrice = pd.ewma(pdat,span=624)
-mavgVol = pd.ewma(vdat,span=624)
-mavgTrade = pd.ewma(tdat,span=624)
+mavgPrice = pd.DataFrame(pd.ewma(pdat,span=624))
+mavgVol = pd.DataFrame(pd.ewma(vdat,span=624))
+mavgTrade = pd.DataFrame(pd.ewma(tdat,span=624))
 
 # Three Latest Data Sets  - Data Frames 
 p24dat = pd.read_csv('price24.csv').iloc[:,1:].values
@@ -96,6 +99,60 @@ v24params = getMarketParameters('volume24.csv')
 t24params = getMarketParameters('trades24.csv')
 
 # Get the 24hr Moving Avg 
-mavgP24 = pd.ewma(p24dat, span=360)
-mavgV24 = pd.ewma(v24dat, span=360)
-mavgT24 = pd.ewma(t24dat, span=360)
+mavgP24 = pd.DataFrame(pd.ewma(p24dat, span=360))
+mavgV24 = pd.DataFrame(pd.ewma(v24dat, span=360))
+mavgT24 = pd.DataFrame(pd.ewma(t24dat, span=360))
+
+# First going to consider the algorithm for 3 Mkts. 
+cbP = pd.DataFrame(analyzeSingleMarket(priceParams.index('coinbase'),1))
+cbV = pd.DataFrame(analyzeSingleMarket(volParams.index('coinbase'),2))
+cbT = pd.DataFrame(analyzeSingleMarket(tradeParams.index('coinbase'),3))
+# Do the same for Bitfinex
+bxP = pd.DataFrame(analyzeSingleMarket(priceParams.index('bitfinex'),1))
+bxV = pd.DataFrame(analyzeSingleMarket(volParams.index('bitfinex'),2))
+bxT = pd.DataFrame(analyzeSingleMarket(tradeParams.index('bitfinex'),3))
+# Do BitStamp last 
+bsP = pd.DataFrame(analyzeSingleMarket(priceParams.index('bitstamp'),1))
+bsV = pd.DataFrame(analyzeSingleMarket(volParams.index('bitstamp'),2))
+bsT = pd.DataFrame(analyzeSingleMarket(tradeParams.index('bitstamp'),3))
+#Compare Coinbase with Bitfinex
+cbp_bxp = cbP.corrwith(bxP,'index')[0]
+cbv_bxv = cbV.corrwith(bxV,'index')[0]
+cbt_bxt = cbT.corrwith(bxT,'index')[0]
+# compare Coinbase w/ itself 
+cbp_cbv = cbP.corrwith(1/cbV,'index')[0]
+cbv_cbt = cbV.corrwith(cbT,'index')[0]
+cbp_cbtv = cbP.corrwith(cbT/cbV,'index')[0]
+
+# I'm guessing Bitstamp correlates MORE with CB than BFX
+cbp_bsp = cbP.corrwith(bsP,'index')[0]
+cbv_bsv = cbP.corrwith(bsV,'index')[0]
+cbt_bst = cbT.corrwith(bsT,'index')[0]
+# compare Bitfinex w/ itself 
+bxp_bxv = bxP.corrwith(bxV,'index')[0]
+
+# Print the results of the comparisons
+print('CB Price <-> BFX Price: %f %s' % (cbp_bxp,"%"))
+print('CB Vol <-> BFX Vol: %f %s' % (cbv_bxv,"%"))
+print('CB Trades/Min <-> BFX Trades/Min: %f %s' % (cbt_bxt,"%"))
+print('---- Comparing Coinbase Stats w/ itself ----')
+print('CB Price <-> CB Vol: %f %s' % (cbp_cbv,"%"))
+print('CB Vol <-> CB Trades/Min: %f %s' % (cbv_cbt,"%"))
+print('CB Price <-> CB [Trades/Volume]: %f %s' %(cbp_cbtv,"%"))
+print('---- Comparing CoinBase with BitStamp ----')
+print('CB Price <-> BS Price: %f %s' % (cbp_bsp,'%'))
+print('CB Vol <-> BS Vol: %f %s' % (cbv_bsv,'%'))
+print('CB Trading <-> BS Trading: %f %s' % (cbt_bst,'%'))
+
+
+def investigateCorr(df1,df2,label1,label2):
+    plt.figure(1)
+    plt.plot(df1,label=label1)
+    plt.plot(df2,label=label2)
+    plt.show()
+    return 0 
+
+
+# Also Need to Define a Cost Function! 
+# Minimize the corr of diffs of df's, e.g: find downhill
+    
